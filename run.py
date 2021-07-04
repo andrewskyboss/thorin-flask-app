445297f8365dbd9975163adb6dfd8fcac2ba187f
andrewskyboss <andrew.cybossky@gmail.com>
2021-07-04 20:55:47 +0000

    Creating HTML pages

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,11 +7,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        port=int(os.environ.get("PORT", "5432")),
         debug=True)
